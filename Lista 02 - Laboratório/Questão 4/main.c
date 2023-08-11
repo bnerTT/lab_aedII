9bd1933030bc553ed6960ae269896f13d74301cd
SrVlady <vladguedes@hotmail.com>
2023-08-11 17:46:17 -0300

    ft: exercicio 4

--- Lista 02 - Laboratório/Questão 4/main.c
+++ Lista 02 - Laboratório/Questão 4/main.c
@@ -1,27 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct vector {
+    int fator;
+    struct vector * no;
+}Vector;
+
+Vector * insere(Vector * f, int j) {
+    Vector * novo = (Vector*) malloc(sizeof(Vector));
+    novo->fator = j;
+    novo->no = f;
+
+    return(novo);
+}
 
 
 void isPerfect(int parametro){
     int soma = 0;
-    int numeros[100];
     for(int i = 0; i <= parametro; i++){
+        Vector * fatores = (Vector*) malloc(sizeof(Vector));
         for(int j = 0; j < i; j++){
             if(i % j == 0){
+                insere(fatores, j);
                 soma += j;
-                
             }
         }
         if(soma == i){
-            printf("%i\t", soma);
+            for(;fatores->no != NULL; fatores = fatores->no) {
+                printf("%d +", fatores->fator);
+                if(fatores->no == NULL) {
+                    printf("%d = ", fatores->fator);
+                    printf("%d\n\n", soma);
+                }
+            }
         }
         soma = 0;
+        free(fatores);
     }
 }
 
 
 
 int main(){
-    int numero = 14;
+    int numero = 50;
     isPerfect(numero);
 
     return 0;
