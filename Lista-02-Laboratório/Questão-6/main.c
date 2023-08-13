fb370670daef0cb57bfcf8f2c92c717d9263d733
SrVlady <vladguedes@hotmail.com>
2023-08-13 14:24:56 -0300

    ft: exercicio 6

--- Lista-02-Laboratório/Questão-6/main.c
+++ Lista-02-Laboratório/Questão-6/main.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+
+int inverterNumero(int valor) {
+    int inverso = 0;
+    
+    do {
+        int digito = valor % 10;
+        inverso = inverso * 10 + digito;
+        valor /= 10;
+    }
+    while(valor > 0);
+    
+    return inverso;
+}
+
+int recursivo(int valor) {
+    if(valor == 0) {
+        return(0);
+    }
+
+    return()
+}
+
+int main(void) {
+    int valor, inverso;
+    printf("Digite um valor inteiro: ");
+    scanf("%d",  &valor);
+    inverso = inverterNumero(valor);
+    printf("O valor invertido: %d\n", inverso);
+
+    return 0;
+}
